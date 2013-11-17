a9acff9adcd00018db3540a77312314689f26210
Israel Calvete <icalvete@gmail.com>
2013-11-17 09:01:14 +0100

    Update puppet repo onlu one time

--- manifests/debian/install.pp
+++ manifests/debian/install.pp
@@ -13,13 +13,15 @@ class common::debian::install inherits common::debian {
     cwd     => '/tmp/',
     command => "/usr/bin/dpkg -i ${common::debian::puppetlabs_package}",
     require => Common::Down_resource['puppetlabs_debian_get_package'],
-    notify  => Exec['puppetlabs_debian_config_package']
+    notify  => Exec['puppetlabs_debian_config_package'],
+    unless  => '/usr/bin/test -f /etc/apt/sources.list.d/puppetlabs.list'
   }
 
   exec {'puppetlabs_debian_config_package':
-    cwd     => '/tmp/',
-    command => '/usr/bin/apt-get update',
-    require => Exec['puppetlabs_debian_install_package'],
+    cwd         => '/tmp/',
+    command     => '/usr/bin/apt-get update',
+    require     => Exec['puppetlabs_debian_install_package'],
+    refreshonly => true,
   }
 
   case $::operatingsystemrelease {
