c3323e63f9bbc12767861ff8cecf2b0e10ff7131
Israel Calvete <icalvete@gmail.com>
2014-10-11 12:35:38 +0200

    adding virtual packages to vagrant

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -3,8 +3,11 @@ class common::vagrant () inherits common::vagrant::params {
   anchor {'common::vagrant::begin':
     before => Class['common::vagrant::install']
   }
-  class {'common::vagrant::install':
+  class {'common::virtual':
     require => Anchor['common::vagrant::begin']
+  }
+  class {'common::vagrant::install':
+    require => CLass['common::virtual']
   }
   class {'common::vagrant::config':
     require => Class['common::vagrant::install']
