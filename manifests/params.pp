c10f92554aeb3016304070e1aedd475da7a04c21
Israel Calvete <icalvete@gmail.com>
2017-03-12 17:01:39 +0100

    Full refactor.

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,10 +12,18 @@ class common::params {
     }
   }
 
-  $backup_dir                = hiera('backup_dir')
-  $cron_mailto               = hiera('cron_mailto')
+  $backup_dir                = hiera('backup_dir', '/srv/backup')
+  $cron_mailto               = hiera('cron_mailto', 'icalvete@gmail.com')
 
-  $redhat_epel_package       = hiera('redhat_epel_package')
-  $redhat_puppetlabs_package = hiera('redhat_puppetlabs_package')
-  $redhat_elrepo_package     = hiera('redhat_elrepo_package')
+  $redhat_epel_package       = hiera(
+    'redhat_epel_package',
+    'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+  )
+  $redhat_puppetlabs_package = hiera(
+    'redhat_puppetlabs_package',
+    'http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm')
+  $redhat_elrepo_package     = hiera(
+    'redhat_elrepo_package',
+    'http://elrepo.org/elrepo-release-6-5.el6.elrepo.noarch.rpm'
+  )
 }
